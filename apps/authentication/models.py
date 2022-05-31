a00155733a7d4762d764b0042e708a20af795851
Bhavneet Singh <46424237+bhavneetsingh58@users.noreply.github.com>
2022-05-31 11:11:53 +0530

    db changes 1

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -17,6 +17,7 @@ class Users(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
     password = db.Column(db.LargeBinary)
+    privelege_type = db.Column(db.String(64))
 
     def __init__(self, **kwargs):
         for property, value in kwargs.items():
